23507caf7f64b597ce0f1438b494629a2664dde0
Tinorus <tinorus91@gmail.com>
2016-03-22 09:52:57 +0100

    Tuto angular

---
+++ Test/Maxime/Frist-app/app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name fristAppApp
+ * @description
+ * # fristAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('fristAppApp', []);
---
+++ Test/Maxime/Frist-app/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fristAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fristAppApp
+ */
+angular.module('fristAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
